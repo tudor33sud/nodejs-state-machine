aada53d7b8199ac5e06c86089a26523a0d32309d
Tudor Suditu <tudor.suditu@siemens.com>
2018-04-20 10:40:39 +0300

    export errors module

---
+++ errors/index.js
@@ -0,0 +1,6 @@
+const StateMachineError = require('./state-machine-error');
+const constants = require('./_constants');
+module.exports = {
+    StateMachineError,
+    errorCodes: constants.errorCodes
+}
