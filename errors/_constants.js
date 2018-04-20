47ace652ef8cf380209a0ba23d4297bb9be3dfbf
Tudor Suditu <tudor.suditu@siemens.com>
2018-04-20 10:40:32 +0300

    added error codes constants

---
+++ errors/_constants.js
@@ -0,0 +1,9 @@
+const errorCodes = {
+    INITIAL_STATE_NOT_FOUND: 'InitialStateNotFound',
+    INVALID_TRANSITION: 'InvalidTransition',
+    RESET_STATE_NOT_FOUND: 'ResetStateNotFound'
+};
+
+module.exports = {
+    errorCodes
+}
